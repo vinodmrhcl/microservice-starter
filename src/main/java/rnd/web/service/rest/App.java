ef8ce74dbb755ee2d4a7d6be9866d0bcbbc7e059
Vinod Kumar <vinodmr@hcl.com>
2018-03-15 13:54:35 +0530

    Included Resource Configuration

--- src/main/java/rnd/web/service/rest/App.java
+++ src/main/java/rnd/web/service/rest/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(new Object[] { AppConfig.class, App.class }, args);
 	}
 
 }

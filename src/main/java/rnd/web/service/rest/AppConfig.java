75d1067faf00cde8aea0fea2a9bd71a8d52fc481
Vinod Kumar <vinodmr@HCL.COM>
2018-03-23 17:27:42 +0530

    Version Upgrade

--- src/main/java/rnd/web/service/rest/AppConfig.java
+++ src/main/java/rnd/web/service/rest/AppConfig.java
@@ -1,7 +1,10 @@
 package rnd.web.service.rest;
 
+import javax.ws.rs.ApplicationPath;
+
 import org.glassfish.jersey.server.ResourceConfig;
 
+@ApplicationPath("/")
 public class AppConfig extends ResourceConfig {
 
 	public AppConfig() {

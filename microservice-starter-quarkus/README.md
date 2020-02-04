d646418cb693eaf9a6b506b66a18e6d1fb196caa
Vinod Kumar <vinodmr@hcl.com>
2020-02-04 13:32:04 +0530

    Added Starter for Red Hat Quarkus

---
+++ microservice-starter-quarkus/README.md
@@ -0,0 +1,1 @@
+# Microservice Starter based on Red Hat Quarkus
---
+++ microservice-starter-quarkus/src/main/java/rnd/web/service/rest/HelloWorldResource.java
@@ -0,0 +1,17 @@
+package rnd.web.service.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/helloworld/")
+public class HelloWorldResource {
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getHello() {
+		return "Hello World";
+	}
+
+}

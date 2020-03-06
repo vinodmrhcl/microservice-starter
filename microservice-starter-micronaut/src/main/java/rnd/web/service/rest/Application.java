0a11b05df3adcd7040485f6dd0eac3a32e48be05
Vinod Kumar <vinodmr@hcl.com>
2020-03-06 09:48:22 +0000

    Added Starter for Micronaut

---
+++ microservice-starter-micronaut/src/main/java/rnd/web/service/rest/Application.java
@@ -0,0 +1,10 @@
+package rnd.web.service.rest;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
